30fdcfda7b7c6da8ae8708ce00dd86fa508b1840
Ashley <200848692+ashley68k@users.noreply.github.com>
2026-01-29 15:21:49 -0500

    Program.cs: entirely gut CLI layer

--- Source/MainWindow.axaml.cs
+++ Source/MainWindow.axaml.cs
@@ -1,6 +1,8 @@
-﻿using ASTRedux.Utils.Logging;
+﻿using ASTRedux.Utils;
+using ASTRedux.Utils.Logging;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
 using Avalonia.Threading;
 
 namespace ASTRedux;
@@ -9,15 +11,31 @@ public partial class MainWindow : Window
     private const int SFX_MODE = 0;
     private const int MUSIC_MODE = 1;
 
+    private FilePickerFileType soundType = new("Dead Rising Sound Files")
+    {
+        Patterns = new[] { "*.rSoundSnd", "*.snd" },
+        AppleUniformTypeIdentifiers = null,
+        MimeTypes = null
+    };
+
+    private FilePickerFileType musicType = new("Dead Rising Music Files")
+    {
+        Patterns = new[] { "*.rSoundAst", "*.ast" },
+        AppleUniformTypeIdentifiers = null,
+        MimeTypes = null
+    };
+
     public MainWindow()
     {
         InitializeComponent();
 
         Logger.OnLogMessage += WriteLog;
-
         Logger.VerbosityLevel = LogDetail.LOW;
 
         SetLabels();
+
+        PluginLoader.LoadPlugins(Environment.CurrentDirectory);
+        Logger.Message("Plugin check complete!", LogType.INFO);
 
         Logger.Message("Welcome to ASTRedux!");
     }
@@ -43,33 +61,94 @@ public partial class MainWindow : Window
     }
 
     // convert Dead Rising format file to standard audio format
-    private void Decode(object? sender, RoutedEventArgs e)
+    private async void Decode(object? sender, RoutedEventArgs e)
     {
         if (ModeTab == null)
             return;
 
+        var topLevel = TopLevel.GetTopLevel(this);
+
         switch (ModeTab.SelectedIndex)
         {
             case SFX_MODE:
+                // file -> folder
+                var soundIn = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+                {
+                    Title = "Select rSoundSnd file to decode from",
+                    FileTypeFilter = new[] { soundType },
+                    AllowMultiple = false
+                });
+                var soundOut = await topLevel.StorageProvider.OpenFolderPickerAsync(new FolderPickerOpenOptions
+                {
+                    Title = "Select folder to decode to",
+                    AllowMultiple = false
+                });
                 break;
             case MUSIC_MODE:
+                // file -> file
+                var musicIn = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+                {
+                    Title = "Select rSoundAst file to decode from",
+                    FileTypeFilter = new[] { musicType },
+                    AllowMultiple = false
+                });
+                var musicOut = await topLevel.StorageProvider.SaveFilePickerAsync(new FilePickerSaveOptions
+                {
+                    Title = "Select audio file to decode to"
+                });
                 break;
         }
+
+        ConversionComplete();
     }
 
     // create a file of Dead Rising format
-    private void Encode(object? sender, RoutedEventArgs e)
+    private async void Encode(object? sender, RoutedEventArgs e)
     {
         // just to be safe
         if (ModeTab == null)
             return;
 
+        var topLevel = TopLevel.GetTopLevel(this);
+
         switch (ModeTab.SelectedIndex)
         {
             case SFX_MODE:
+                // folder -> file
+                var soundIn = await topLevel.StorageProvider.OpenFolderPickerAsync(new FolderPickerOpenOptions
+                {
+                    Title = "Select folder to encode rSound from",
+                    AllowMultiple = false
+                });
+                var soundOut = await topLevel.StorageProvider.SaveFilePickerAsync(new FilePickerSaveOptions
+                {
+                    Title = "Select rSound file to encode to"
+                });
                 break;
             case MUSIC_MODE:
+                // file -> file
+                var musicIn = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+                {
+                    Title = "Select audio file to encode from",
+                    FileTypeFilter = new[] { musicType },
+                    AllowMultiple = false
+                });
+                var musicOut = await topLevel.StorageProvider.SaveFilePickerAsync(new FilePickerSaveOptions
+                {
+                    Title = "Select AST file to encode to"
+                });
                 break;
+        }
+
+        ConversionComplete();
+    }
+
+    private void ConversionComplete()
+    {
+        if (Logger.VerbosityLevel > LogDetail.LOW)
+        {
+            File.WriteAllText($"{DateTime.Now:yyyy-MM-dd_HH-mm-ss}.log", Logger.LogOut.ToString());
+            Logger.Message("Log file written!", LogType.INFO);
         }
     }
 
